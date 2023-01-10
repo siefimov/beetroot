54da5516a3c9eda7e951c8eff41a24b9878e445a
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-01-10 21:40:33 +0200

    added styles for dark theme

--- lesson19-r-nav/homework19-resp_nav/js/script.js
+++ lesson19-r-nav/homework19-resp_nav/js/script.js
@@ -1,8 +1,16 @@
 const chevronBox = document.querySelector(".chevron-box");
 const sidebar = document.querySelector(".sidebar");
 const chevron = document.querySelector(".chevron");
+const body = document.querySelector('body');
+const switchTheme = document.querySelector('.switch-toggle');
+
+
 
 chevronBox.addEventListener("click", () => {
   sidebar.classList.toggle("close");
   chevron.classList.toggle("switch");
 });
+
+switchTheme.addEventListener('click',()=>{
+  body.classList.toggle("dark");
+})
