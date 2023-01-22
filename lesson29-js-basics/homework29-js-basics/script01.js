ced69a247d403cae5a9a595e807b87c9736ff2cf
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-01-22 23:22:25 +0200

    basics js

---
+++ lesson29-js-basics/homework29-js-basics/script01.js
@@ -0,0 +1,34 @@
+//* correct names of variables
+const fullName = "Peter Angel";
+const nameAndSurname = "John Clinton";
+const clientsName = "Alisa Olush";
+const my_name = "Kevin Costner";
+console.log(my_name);
+
+let number = 23;
+let name = "John";
+let Name = "John";
+let NAME = "John";
+let message = "Hello!";
+let _password = "12ty984#!0#gfhf";
+const $age = 23;
+const $ = true;
+const _ = true;
+
+//* ==== incorrect names of variables ===== */
+// let 1name = "Peter";
+// let !message = "Welcome";
+// let my-name = "Ben";
+// let -auto = "BMW";
+// let :-) = false;
+// let 55555 = false;
+// let return = 5;
+
+//* COMMENTS
+// 1. possible ways to comment the code
+/* 2. possible ways to comment the code */
+
+//* Styles writing names of variables
+// camelCase
+// PascalCase
+// snake_case
---
+++ lesson29-js-basics/homework29-js-basics/script02.js
@@ -0,0 +1,15 @@
+// let userName = prompt('Your name?');
+// alert(`Hello, ${userName}`);
+
+
+const YEAR = 2023;
+
+let yearOfBirth = prompt("Your birth year?");
+let age = YEAR - +yearOfBirth;
+alert(`Your age is ${age}`);
+
+
+let lengthOfSquare = prompt("Enter length of square");
+let perimeter = +lengthOfSquare * 4;
+alert(`Perimeter of a square is ${perimeter}`);
+
