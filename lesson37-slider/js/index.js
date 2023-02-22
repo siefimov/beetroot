5e8df8ead189ac0d1dd002987f1028c3e02f654a
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-02-22 13:18:29 +0200

    added section partners

--- lesson37-slider/js/index.js
+++ lesson37-slider/js/index.js
@@ -20,8 +20,104 @@ navBtn.addEventListener("click", () => {
 
 const swiper = new Swiper(".swiper-slider", {
   loop: true,
+  speed: 800,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+});
+
+const swiperProduct = new Swiper(".swiper-product", {
+  // loop: true,
+  speed: 800,
+  spaceBetween: 20,
+
+  pagination: {
+    el: ".swiper-pagination__product",
+    dynamicBullets: true,
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + "</span>";
+    },
+  },
+  // + (index + 1)
+  navigation: {
+    nextEl: ".product__next-btn",
+    prevEl: ".product__prev-btn",
+  },
+
+  breakpoints: {
+    1320: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    769: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    488: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    487: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
+});
+
+const swiperPartners = new Swiper(".swiper-partners", {
+  // loop: true,
+  speed: 800,
+  slidesPerView: 9,
+  spaceBetween: 20,
+
+  navigation: {
+    nextEl: ".partners__arr-next-button",
+    prevEl: ".partners__arr-prev-button",
+  },
+
+  pagination: {
+    el: ".swiper-pagination__partners",
+    dynamicBullets: true,
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + "</span>";
+    },
+  },
+
+  breakpoints: {
+    1320: {
+      slidesPerView: 9,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 8,
+      spaceBetween: 10,
+    },
+    769: {
+      slidesPerView: 8,
+      spaceBetween: 10,
+    },
+    // 488: {
+    //   slidesPerView: 6,
+    //   spaceBetween: 10,
+    // },
+    // 487: {
+    //   slidesPerView: 6,
+    //   spaceBetween: 10,
+    // },
+    320: {
+      slidesPerView: 8,
+      spaceBetween: 10,
+    },
   },
 });
